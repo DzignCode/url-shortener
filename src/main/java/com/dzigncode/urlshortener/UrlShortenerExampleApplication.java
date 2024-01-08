668e3bdf9b11aae754361c19c53b0b790bb6f449
Ankit Jain <ankitjain@PINEMAC0214.local>
2024-01-08 11:38:41 +0530

    URL shortener using Redis & Guava

---
+++ src/main/java/com/dzigncode/urlshortener/UrlShortenerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.dzigncode.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerExampleApplication.class, args);
+	}
+}
